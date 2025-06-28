13e406b6f5ab09db4a780db9cd5351b32e9dcefb
Amol Chavan <amolchavan777@gmail.com>
2025-06-28 23:39:00 +0300

    Ensure UTF-8 when reading and writing

--- src/main/java/com/modeler/app/GraphMLExporter.java
+++ src/main/java/com/modeler/app/GraphMLExporter.java
@@ -1,12 +1,15 @@
 package com.modeler.app;
 
-import java.io.FileWriter;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class GraphMLExporter {
     public static void export(Map<String, Set<String>> model, String filePath) throws IOException {
-        try (FileWriter fw = new FileWriter(filePath)) {
+        try (BufferedWriter fw = Files.newBufferedWriter(Paths.get(filePath), StandardCharsets.UTF_8)) {
             fw.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
             fw.write("<graphml xmlns=\"http://graphml.graphdrawing.org/xmlns\">\n");
             fw.write("  <graph id=\"G\" edgedefault=\"directed\">\n");
